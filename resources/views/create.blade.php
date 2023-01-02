f744182d9028829c2fb058614d4a43f71faa4633
Yasz Avellia <zakamaragames@gmail.com>
2023-01-02 22:05:18 +0700

    menambah tampil data

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,12 +1,15 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Create Guru</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col-6">
@@ -19,7 +22,7 @@
                                 <label>Name</label>
                                 <input type="text" name="name" class="form-control" autocomplete="off">
                             </div>
-                            
+
                             <div class="mt-3">
                                 <label>Age</label>
                                 <input type="number" name="age" class="form-control" autocomplete="off">
@@ -31,21 +34,53 @@
                                     <option disabled selected>Choose One Gender</option>
                                     <option value="M">Laki Laki</option>
                                     <option value="W">Perempuan</option>
-                                  </select>
+                                </select>
                             </div>
                             <div class="mt-3">
                                 <label>Alamat</label>
                                 <input type="text" name="alamat" class="form-control" autocomplete="off">
                             </div>
                             <div class="mt-3">
-                                <button class="btn btn-primary" name="submit" value="sbt" type="submit">Submit</button>
+                                <button class="btn btn-primary" name="submit" value="sbt"
+                                    type="submit">Submit</button>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="row mt-5 justify-content-center">
+                <div class="col-11">
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Name</th>
+                                <th>Age</th>
+                                <th>Gender</th>
+                                <th>Alamat</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($guru as $data)
+                                
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$data->name}}</td>
+                                <td>{{$data->age}}</td>
+                                <td>{{$data->gender}}</td>
+                                <td>{{$data->alamat}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
